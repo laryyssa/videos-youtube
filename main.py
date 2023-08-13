fe819f73fc8f6c66f207df1315a2b28ef031ec12
Laryssa Paiva <laryssapaiva@id.uff.br>
2023-08-13 12:08:22 -0300

    updates

--- main.py
+++ main.py
@@ -1,75 +1,88 @@
 import pytube
 from pytube import YouTube
 from pytube.exceptions import AgeRestrictedError
-import os
 
-def limpar_terminal():
+import os
+import traceback
+
+def clear_terminal():
     if os.name == "nt":  # Windows
         os.system("cls")
-    # else:  # Linux ou macOS
-    #     os.system("clear")
+    else:  # Linux ou macOS
+        os.system("clear")
 
-def verifyLink(url):
+def download_video(video_url, path_folder):
+    yt = YouTube(video_url)
+    video = yt.streams.get_highest_resolution()
+    print('Baixando...')
+    video.download(path_folder)
+    print('Download Completo!') 
+
+def verify_link(url):
     try:
         yt = pytube.YouTube(url)
         return True
     except pytube.exceptions.VideoUnavailable:
+        print('Link Inválido!')
         return False
 
-def dowloadVideo(path_folder):
-    var = 1
+def verify_path(path):
+    if os.path.exists(path):
+        return True
+    else:
+        print('Caminho de pasta inválido!')
+        return False
 
-    while var !=0:
-        video_url = input('Insira o Link do vídeo: ')
-        
-        if verifyLink(video_url):
-            try:
-                yt = YouTube(video_url)
-                video = yt.streams.get_highest_resolution()
+def download_helper():
+    # path = input('Insira o caminho da pasta: ')
+    path_folder = r'C:\Users\larys\OneDrive\Área de Trabalho\videos'
+    video_url = input('Insira o Link do vídeo: ')
+    
+    if verify_link(video_url) and verify_path(path_folder):
+        try:
+            download_video(video_url, path_folder)              
+        except Exception:
+            print(traceback.format_exc())
+    
+    else:
+        clear_terminal()
 
-                print('Baixando...')
-                video.download(path_folder)
-                print('Download Completo!') 
-                var = input('Deseja baixar mais video? [1] Sim [0] Não => ')
-                break               
+    var = input('Baixar mais vídeos? [0] - Não [1] - Sim')
+    return var
 
-            except AgeRestrictedError as e:
-                yt = YouTube(video_url, use_oauth=False,  allow_oauth_cache=True)
-                video = yt.streams.get_highest_resolution()
-                print('Baixando...')
-                video.download(path_folder)
-                print('Download Completo!') 
-                break 
 
-        else:
-            print('Link inválido!')
-            var = input('[0] - Sair [1] - Continuar')
-            limpar_terminal()
+
+
+def menu():
+    print('-------------------')
+    print('[0] - Sair\n[1] - Baixar Vídeo\n[2] - Listar Vídeos')
+    print('-------------------')
+
+def show_action(user_action):
+    print('-------------------')
+    print(user_action)
+    print('-------------------')
 
 def main():
-    var = 1
-    while var != 0:
-        print('-------------------')
-        print('[0] - Sair\n[1] - Baixar Vídeo')
-        print('-------------------')
+    while True:
+        menu()
+
         var = input('Digite sua opção: ')
-        limpar_terminal()
+        clear_terminal()
 
         if var == '0':
             print('Saindo...')
             break
         elif var == '1':
-            limpar_terminal()
-            # path = input('Insira o caminho da pasta: ')
-            # path = r'C:\Users\larys\OneDrive\Documentos\Projetos\Videos Youtube\downloaded_videos'
-            path = r'C:\Users\larys\OneDrive\Área de Trabalho\videos'
-            dowloadVideo(path)
+            show_action('BAIXAR VIDEO')
+            var = download_helper()
         elif var == '2':
             # listar videos que já tinham e novos
+            show_action('LISTAR VIDEOS')
             print('2-listar')
         else:
-            print('Opção Inválida!')
+            print('Opção Inválida! Tente Novamente!')
 
-# main()
+main()
 
-dowloadVideo(r'C:\Users\larys\OneDrive\Área de Trabalho\videos')
+# download_helper(r'C:\Users\larys\OneDrive\Área de Trabalho\videos')
